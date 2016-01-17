9f1ae9eace339c36d87a36f1c069a93cc41a16f3
Ben Collier <bmcollier@gmail.com>
2016-01-17 16:09:16 +0000

    Summaries now listed with latest article at top.

--- glycon_summaries/models.py
+++ glycon_summaries/models.py
@@ -1,25 +1,24 @@
 """ Provides extra models for page summary views and summary blocks
 """
 from django.db import models
-
 from glycon.models import BaseContent, BaseBlock
 from glycon_summaries.views import summary_block_html
 
 
 class BaseSummary(models.Model):
-
     tag_filter = models.CharField(max_length=255)
 
     @property
     def filter_items(self):
-        items = BaseContent.objects.filter(tags__text=self.tag_filter).filter(published=True).all()[0:4]
+        items = BaseContent.objects.filter(tags__text=self.tag_filter).filter(published=True).all().order_by(
+            '-create_dt')[0:4]
         return items
 
     class Meta:
         abstract = True
 
-class Summary(BaseSummary, BaseContent):
 
+class Summary(BaseSummary, BaseContent):
     def __str__(self):
         return self.title
 
@@ -28,7 +27,6 @@ class Summary(BaseSummary, BaseContent):
 
 
 class SummaryBlock(BaseSummary, BaseBlock):
-
     def content(self, request=None):
         return summary_block_html(self)
 
